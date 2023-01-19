25ef379373ef1c57071d175031084c0443c1593a
lera <leraaler3@gmail.com>
2023-01-19 15:16:02 +0700

    test commit

--- och/Program.cs
+++ och/Program.cs
@@ -1,4 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, hfgbhnj");
-Console.WriteLine("лералиана");
+﻿class Program
+
+{ static void Main(string[] args)
+    {
+        double a, b, z;
+        
+        Console.WriteLine("первое число");
+        a = Convert.ToDouble(Console.ReadLine()); 
+        
+        Console.WriteLine("второе число"); 
+        b = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("Выберите число: 1. Выполнить сложение. 2. Выполнить вычитание. 3. Выполнить умножение. 4. Выполнить деление");
+        z = Convert.ToInt32(Console.ReadLine());
+
+
+        switch (z)
+
+        {
+            case 1:
+                Console.WriteLine(a + b);
+                break;
+
+            case 2:
+                Console.WriteLine(a - b);
+                break;
+
+            case 3:
+                Console.WriteLine(a * b);
+                break;
+
+            case 4:
+                Console.WriteLine(a / b);
+                break;
+
+
+            default:
+                Console.WriteLine("такого варанта нет");
+                break;
+        }
+        Console.WriteLine();
